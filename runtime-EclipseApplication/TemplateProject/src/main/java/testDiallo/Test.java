08666e5b0fcef42da80635afc02e3ec357d338b9
Bubakr <bubakar24@gmail.com>
2019-12-16 12:49:49 +0100

    add classe HelloWorld

---
+++ runtime-EclipseApplication/TemplateProject/src/main/java/testDiallo/Test.java
@@ -0,0 +1,13 @@
+package testDiallo;
+
+public class Test {
+	
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String helloWorld(String world) {
+		return ("Hello "+world);
+	}
+
+}
